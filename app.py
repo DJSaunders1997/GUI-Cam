b57c3d72d8a6c7ca613143accae6f8710e814100
Dave <DJSaunders1997@gmail.com>
2020-10-23 21:30:12 +0100

    Inital V2 setup with test video

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
 #https://flask.palletsprojects.com/en/1.1.x/quickstart/
 # https://kubernetes.io/blog/2019/07/23/get-started-with-kubernetes-using-python/
 
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+from camera import Camera
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('main.html') # Pass parameters here  # Pass video object here.
+    return render_template('index.html') # Pass parameters here  # Pass video object here.
+
+# https://blog.miguelgrinberg.com/post/video-streaming-with-flask
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/old')
+def old():
+    return render_template('old.html') # Pass parameters here  # Pass video object here.
+
 
 @app.route("/who")
 def hello():
     return "Created by David Saunders"
-
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
