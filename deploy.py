2fb69bd1f62b700588223d8c36d55ee0db1f39db
Dave <DJSaunders1997@gmail.com>
2020-10-28 20:28:36 +0000

    Fixed toggle using python requests

--- deploy.py
+++ deploy.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, Response, url_for
+import requests
 
 app = Flask(__name__)
+piIP = 'http://192.168.68.135:5000' # Needs the http so requests knows how to connect
+
 
 @app.route('/')
 @app.route('/index')
@@ -11,6 +14,17 @@ def index():
 def old():
     return render_template('deployold.html')
 
+#turn on green LED
+@app.route('/toggle_red_led')
+def toggle_red_led():
+    x = requests.get(f'{piIP}/toggle_red_led')
+    return (x)
+
+#turn on red LED
+@app.route('/toggle_green_led')
+def toggle_green_led():
+    x = requests.get(f'{piIP}/toggle_green_led')
+    return (x)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
